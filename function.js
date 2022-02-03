077ba6f1bc7e3fefd4c798f4dd8b8806b1354d94
NMckaufman <nisa_alise@verizon.net>
2022-02-02 19:53:26 -0800

    Likes

---
+++ function.js
@@ -0,0 +1,23 @@
+var likesCount = document.querySelector('#likesCount')
+console.log(likesCount);
+
+function increaseCount(){
+        likesCount.innerText++;
+
+}
+
+var likesCount2 = document.querySelector('#likesCount2')
+console.log(likesCount);
+
+function increaseCount(){
+        likesCount2.innerText++;
+
+}
+
+var likesCount3 = document.querySelector('#likesCount3')
+console.log(likesCount);
+
+function increaseCount(){
+        likesCount3.innerText++;
+
+}
